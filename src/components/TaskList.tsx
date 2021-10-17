5fbc2d7d6c82cea0a2396e50e3d0c8fd1d0e038d
RaimundooNeto <raimundononatobatistaneto@gmail.com>
2021-10-17 16:04:31 -0300

    refatoração das funções de adicão de nova task, conclusão de task e exclusão de task.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -24,37 +24,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if(newTaskTitle) {
-      setTasks([
-        ...tasks,
-        {
-          id: random(),
-          title: newTaskTitle,
-          isComplete: false
-        }
-      ]);
-      setNewTaskTitle('');
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: random(),
+      title: newTaskTitle,
+      isComplete: false
     }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    tasks.forEach(task => {
-      if(task.id == id) {
-        task.isComplete = !task.isComplete;
-        setTasks([...tasks]);
-      }
-    });
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    tasks.forEach((task, index) => {
-      if(task.id == id) {
-        tasks.splice(index, 1);
-        setTasks([...tasks]);
-      }
-    });
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
